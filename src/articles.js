0ac46dfbdff7b3c6e70d17ebebbd6aa30a76fc03
Zerrelius <Jeremy.Held@web.de>
2025-01-10 10:30:22 +0100

    Modified main.js

--- src/articles.js
+++ src/articles.js
@@ -1,4 +1,4 @@
-const articles = [
+export const articles = [
   {
     id: 1,
     title: "I ♥ Javascript",
@@ -36,30 +36,96 @@ const articles = [
     id: 2,
     title: "CSS is Awesome",
     content: "This is the content of article 2",
-    image: "love.jpg",
+    image: {
+      src: "love.jpg",
+      alt: "Picture with Love",
+    },
     teaser: "This is the reason why I love CSS!",
     date: "2021-01-02",
     author: "Jane Doe",
-    tags: ["Design", "Research", "Presentation"],
+    tags: [
+      {
+        id: 1,
+        name: "Design",
+        bgColor: "#F9F5FF",
+        textColor: "#6941C6",
+      },
+      {
+        id: 2,
+        name: "Research",
+        bgColor: "#EEF4FF",
+        textColor: "#3538CD",
+      },
+      {
+        id: 3,
+        name: "Presentation",
+        bgColor: "#FDF2FA",
+        textColor: "#C11574",
+      },
+    ],
   },
   {
     id: 3,
     title: "HTML is Cool",
     content: "This is the content of article 3",
-    image: "nodejs.jpg",
+    image: {
+      src: "nodejs.jpg",
+      alt: "Picture with code",
+    },
     teaser: "This is the reason why I love HTML!",
     date: "2021-01-03",
     author: "John Doe",
-    tags: ["Design", "Research", "Presentation"],
+    tags: [
+      {
+        id: 1,
+        name: "Design",
+        bgColor: "#F9F5FF",
+        textColor: "#6941C6",
+      },
+      {
+        id: 2,
+        name: "Research",
+        bgColor: "#EEF4FF",
+        textColor: "#3538CD",
+      },
+      {
+        id: 3,
+        name: "Presentation",
+        bgColor: "#FDF2FA",
+        textColor: "#C11574",
+      },
+    ],
   },
   {
     id: 4,
     title: "React is the Best",
     content: "This is the content of article 4",
-    image: "remote.jpg",
+    image: {
+      src: "remote.jpg",
+      alt: "View from window to a river",
+    },
     teaser: "This is the reason why I love React!",
     date: "2021-01-04",
     author: "Jane Doe",
-    tags: ["Design", "Research", "Presentation"],
+    tags: [
+      {
+        id: 1,
+        name: "Design",
+        bgColor: "#F9F5FF",
+        textColor: "#6941C6",
+      },
+      {
+        id: 2,
+        name: "Research",
+        bgColor: "#EEF4FF",
+        textColor: "#3538CD",
+      },
+      {
+        id: 3,
+        name: "Presentation",
+        bgColor: "#FDF2FA",
+        textColor: "#C11574",
+      },
+    ],
   },
 ];
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import articles from "./articles.js";
+import { articles } from "./articles.js";
 
 const container = document.querySelector("#container");
 
 articles.forEach((article) => {
-  container.innerHTML = `
+  container.innerHTML += `
           <div class="border lg:row-span-2 shadow-md">
             <a href="${article.link}">
               <img class="object-cover w-full h-80" src="${article.image.src}" alt="${article.image.alt}" />
@@ -13,10 +13,8 @@ articles.forEach((article) => {
                 <p class="mt-4">${article.teaser}</p>
                 <ul class="flex gap-2 mt-4">
                 <!-- DRY Principle -->
-                  <li><span class="px-2 py-1 rounded text-sm mr-2 bg-[${article.tags[0].bgColor}] text-[${article.tags[0].textColor}]">${article.tags[0].name}</span></li>
-                  <li><span class="px-2 py-1 rounded text-sm mr-2 bg-[${article.tags[1].bgColor}] text-[${article.tags[1].textColor}]">${article.tags[1].name}</span></li>
-                  <li><span class="px-2 py-1 rounded text-sm mr-2 bg-[${article.tags[2].bgColor}] text-[${article.tags[2].textColor}]">${article.tags[2].name}</span></li>
-                </ul>
+                  ${article.tags.map(tag => `<li><span class="px-2 py-1 rounded text-sm mr-2 bg-[${tag.bgColor}] text-[${tag.textColor}]">${tag.name}</span></li>`)}
+                  </ul>
               </div>
             </a>
           </div>`;
